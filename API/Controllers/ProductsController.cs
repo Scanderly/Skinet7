934f74d59612802288db219966e91758003a243a
2ra190scan@gmail.com <2ra190scan@gmail.com>
2023-05-28 04:49:03 +0400

    End of section 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,13 +6,13 @@ using Core.Interfaces;
 using Core.Specifications;
 using API.Dtos;
 using AutoMapper;
+using API.Errors;
 
 namespace API.Controllers
 {
 
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    
+    public class ProductsController : BaseApiController
     {
 
         private readonly IGenericRepository<Product> _productsRepo;
@@ -39,10 +39,19 @@ namespace API.Controllers
             return Ok(_mapper.Map<IReadOnlyList<Product>,IReadOnlyList<ProductToReturnDto>>(products));
         }
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
-            var spec= new ProductWithTypesAndBrandsSpecification();
+            var spec= new ProductWithTypesAndBrandsSpecification(id);
+
             var product= await _productsRepo.GetEntityWithSpec(spec);
+
+            if(product==null)
+            {
+                return NotFound(new ApiResponse(404));
+            }
+
             return _mapper.Map<Product,ProductToReturnDto>(product);
         }
         [HttpGet("brands")]
